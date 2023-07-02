5a4ca6ab65a3623fc87b60d42132322b3989c7a0
Davron01~ <d.mirabdullayev@mail.ru>
2023-07-03 03:09:04 +0500

    nextjs and tailwind darkmode

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="dark:text-red-700 bg-slate-900 dark:bg-green-800 text-white">
+      <h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus culpa
+        facere voluptate vero molestias inventore eligendi soluta perspiciatis
+        neque totam ipsam, mollitia quae voluptates nulla aliquam. Vitae dolore
+        eum qui.
+      </h3>
+    </main>
+  );
+}
